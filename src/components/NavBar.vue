680b7a747cbeeb02ffd8b33ec057bd765abb0d29
Dani-Alarcon <daniel.alarcon.7e6@itb.cat>
2026-01-22 17:23:25 +0100

    Navbar creado

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,40 +1,37 @@
 <template>
-    <header class="main-header">
-        <nav class="nav-container">
-            <h1 class="logo-text">TechBuilds</h1>
-            <span class="badge">Configurator</span>
+    <header>
+        <nav >
+            <h1>TechBuilds</h1>
+            <span>Configurator</span>
         </nav>
     </header>
 </template>
 
 <style scoped>
-.main-header {
+header {
     background-color: #0D1526;
     width: 100%;
     padding: 15px 0;
 }
 
-.nav-container {
-    max-width: 1200px; 
-    margin: 0 auto;    
+nav {
     display: flex;
     align-items: center;
     gap: 10px;        
-    padding: 0 20px;
+    padding: 0 5px;
 }
 
-.logo-text {
+h1 {
     color: #1e78ff;
     margin: 0;
-    font-size: 1.5rem;
 }
 
-.badge {
+span {
     color: white;
     background-color: blue;
     font-weight: bold;
     padding: 2px 8px;
     border-radius: 4px;
-    font-size: 0.8rem;
+    font-size: 1rem;
 }
 </style>
