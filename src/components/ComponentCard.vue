d68d793296b48102ab263ccf10e4b79486661c35
Dani-Alarcon <daniel.alarcon.7e6@itb.cat>
2026-01-27 17:42:38 +0100

    estilo card de componente

--- src/components/ComponentCard.vue
+++ src/components/ComponentCard.vue
@@ -15,12 +15,36 @@ defineEmits(["addToCart"])
 //const count = ref(0);
 </script>
 <template>
-  <duv class="card">
-    <img :src="`../images/${component.image}`" class="mb-3" width="200" />
+  <div class="card">
+    <img :src="`../images/${component.image}`" class="mb-3" width="200px" />
     
     <p>
       {{ component.name }} - {{component.price}}€
-   
     </p>
-  </duv>
+    <button>Afegir Muntatge</button>
+  </div>
 </template>
+<style scoped>
+.card{
+    background-color: rgb(228, 228, 228);
+    width: 250px;
+    text-align: center;
+    margin-bottom: 10px;
+}
+.card button{
+    width: 200px;
+    background-color: blue;
+    color: white;
+    margin-bottom: 8px;
+}
+.card, .card button{
+    border-radius: 8px;
+    font-weight: bold;
+}
+.card button:hover{
+    background-color: rgb(11, 109, 255);
+}
+.card button:active{
+    background-color: rgb(0, 119, 255);
+}
+</style>
